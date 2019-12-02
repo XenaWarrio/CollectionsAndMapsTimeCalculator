846e078bc300aebf0a8c4f9b3b5199c1f3680179
DanGdl <dan@superup.me>
2019-12-02 11:18:29 +0200

    # fix

--- app/src/test/java/dx/queen/newcalculationandmaps/dto/task/MapTaskDataTest.java
+++ app/src/test/java/dx/queen/newcalculationandmaps/dto/task/MapTaskDataTest.java
@@ -1,11 +1,11 @@
 package dx.queen.newcalculationandmaps.dto.task;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Map;
@@ -18,40 +18,24 @@ public class MapTaskDataTest {
     private final int COUNT_OF_ELEMENTS = 6;
 
     @Mock
-    Map<Integer,Integer> map;
+    Map<Integer, Integer> map;
 
-    MapTaskData taskData;
-
-
+    private MapTaskData taskData;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         taskData = new MapTaskData(map, R.string.add_to_treemapmap);
-
-
     }
-
 
     @Test
     public void fill() {
-        ArgumentCaptor<Map<Integer,Integer>> argumentCaptor = ArgumentCaptor.forClass(Map.class);
+        final ArgumentCaptor<Map<Integer, Integer>> argumentCaptor = ArgumentCaptor.forClass(Map.class);
+
         taskData.fill(COUNT_OF_ELEMENTS);
 
-        map.putAll(argumentCaptor.capture());
+        // DO NOT CALL MOCK'S METHOD IN TESTS!!!!! read and remove
+        // map.putAll(argumentCaptor.capture());
 
-        for (int i = 0; i < COUNT_OF_ELEMENTS; i++) {
-            map.put(i, 3);
-            map.putAll(argumentCaptor.capture());
-
-        }
-        int mapSize = argumentCaptor.getValue().size();
-        Assert.assertEquals(mapSize, COUNT_OF_ELEMENTS);
-
-
-
-
-
-
-
+        Mockito.verifyNoMoreInteractions(map);
     }
 }

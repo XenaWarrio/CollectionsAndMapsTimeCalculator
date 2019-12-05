eae88553cdfeb0f4948a4bc9ca919a824321a761
Xena <queen.de.xena@gmail.com>
2019-12-05 12:07:25 +0200

    Mysterious Mistake

--- app/src/test/java/dx/queen/newcalculationandmaps/TestSuit.java
+++ app/src/test/java/dx/queen/newcalculationandmaps/TestSuit.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite;
 import dx.queen.newcalculationandmaps.dto.task.ListTaskDataTest;
 import dx.queen.newcalculationandmaps.dto.task.MapTaskDataTest;
 import dx.queen.newcalculationandmaps.model.calculator.TimeCalculatorImplTest;
-import dx.queen.newcalculationandmaps.model.supplier.CollectionsTasksSupplier;
-import dx.queen.newcalculationandmaps.model.supplier.MapsTasksSupplier;
+import dx.queen.newcalculationandmaps.model.supplier.CollectionsTasksSupplierTest;
+import dx.queen.newcalculationandmaps.model.supplier.MapsTasksSupplierTest;
 import dx.queen.newcalculationandmaps.ui.fragments.CollectionsPresenterTest;
 
 @RunWith(Suite.class)
@@ -16,8 +16,8 @@ import dx.queen.newcalculationandmaps.ui.fragments.CollectionsPresenterTest;
         TimeCalculatorImplTest.class,
         ListTaskDataTest.class,
         MapTaskDataTest.class,
-        CollectionsTasksSupplier.class,
-        MapsTasksSupplier.class
+        CollectionsTasksSupplierTest.class,
+        MapsTasksSupplierTest.class
 })
 public class TestSuit {
 }

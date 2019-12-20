fad541194cb461d509f65ee1230ee00e008878fc
Xena <queen.de.xena@gmail.com>
2019-12-20 13:27:46 +0200

    MapsAndroidTest

--- app/src/androidTest/java/dx/queen/newcalculationandmaps/ui/fragments/MapsAndroidTest.java
+++ app/src/androidTest/java/dx/queen/newcalculationandmaps/ui/fragments/MapsAndroidTest.java
@@ -40,13 +40,6 @@ import static org.hamcrest.Matchers.allOf;
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MapsAndroidTest {
 
-    private RecyclerView recyclerView;
-    private ViewPager viewPager;
-    private final int FRAGMENT_ID = 1;
-    private final String TEST_RESULT = "3.0ms";
-    private final String DEFAULT_RESULT = "0.0ms";
-
-
     @Rule
     public final ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class) {
         @Override
@@ -56,6 +49,11 @@ public class MapsAndroidTest {
             AppInstance.getInstance().setAppComponent(DaggerAppComponent.builder().appModule(new AppModuleTest(app)).build());
         }
     };
+    private final int FRAGMENT_ID = 1;
+    private final String TEST_RESULT = "3.0ms";
+    private final String DEFAULT_RESULT = "0.0ms";
+    private RecyclerView recyclerView;
+    private ViewPager viewPager;
 
     private void checkSingleElementInRW(int position, int stringResourceId, boolean isCalcComplete) {
         Matcher<View> recyclerAtPosition = new RecyclerViewMatcher(R.id.recycler).atPosition(position);
@@ -79,14 +77,17 @@ public class MapsAndroidTest {
                     .check(selectedDescendantsMatch(isAssignableFrom(ProgressBar.class), withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
         }
     }
+
     private void checkAllElementsInRecyclerView(boolean isCalcComplete) {
-        checkSingleElementInRW(0, R.string.add_to_treemapmap, isCalcComplete);
-        checkSingleElementInRW(1, R.string.add_to_hashmap, isCalcComplete);
-        checkSingleElementInRW(2, R.string.search_treemap, isCalcComplete);
-        checkSingleElementInRW(3, R.string.search_hashmap, isCalcComplete);
+        checkSingleElementInRW(0, R.string.add_to_hashmap, isCalcComplete);
+        checkSingleElementInRW(1, R.string.remove_hashmap, isCalcComplete);
+        checkSingleElementInRW(2, R.string.search_hashmap, isCalcComplete);
+        checkSingleElementInRW(3, R.string.add_to_treemapmap, isCalcComplete);
         checkSingleElementInRW(4, R.string.remove_treemap, isCalcComplete);
-        checkSingleElementInRW(5, R.string.remove_hashmap, isCalcComplete);
+        checkSingleElementInRW(5, R.string.search_treemap, isCalcComplete);
+
     }
+
     private void checkRecyclerViewInLoad() {
         if (recyclerView == null) {
             viewPager = mainActivityActivityTestRule.getActivity().findViewById(R.id.view_pager);
@@ -98,58 +99,60 @@ public class MapsAndroidTest {
                     .check(selectedDescendantsMatch(isAssignableFrom(ProgressBar.class), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
         }
     }
+
     private void testInputOnTab(String countElements, String countThreads) {
         onView(allOf(withId(R.id.et_operations), isDisplayed())).perform(typeText(countElements));
         onView(allOf(withId(R.id.et_threads), isDisplayed())).perform(typeText(countThreads),
                 closeSoftKeyboard());
         onView(allOf(withId(R.id.bt_start), isDisplayed())).perform(click());
     }
+
     @Test
     public void testCollections() throws InterruptedException {
-        onView(withId(R.id.view_pager)).perform(ViewActions.swipeRight());
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft());
         Thread.sleep(500);
         checkAllElementsInRecyclerView(false);
-        testInputOnTab("100000", "6");
+        testInputOnTab("10", "6");
         Thread.sleep(500);
         checkRecyclerViewInLoad();
         Thread.sleep(4000);
         checkAllElementsInRecyclerView(true);
     }
+
     @Test
     public void testInputElementsEmptyError() throws InterruptedException {
-        onView(withId(R.id.view_pager)).perform(ViewActions.swipeRight());
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft());
         Thread.sleep(500);
         testInputOnTab(" ", "6");
         Thread.sleep(200);
-        onView(withId(R.id.et_operations)).check(matches(hasErrorText(String.valueOf(R.string.elements_empty))));
-        // onView(withText(R.string.elements_empty)).inRoot(withDecorView(not( is(mainActivityActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.et_operations), isDisplayed())).check(matches(hasErrorText(mainActivityActivityTestRule.getActivity().getBaseContext().getString(R.string.elements_empty))));
     }
+
     @Test
     public void testInputElementsTooLittleError() throws InterruptedException {
-        onView(withId(R.id.view_pager)).perform(ViewActions.swipeRight());
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft());
         Thread.sleep(500);
         testInputOnTab("0", "6");
         Thread.sleep(200);
-        onView(withId(R.id.et_operations)).check(matches(hasErrorText(String.valueOf(R.string.elements_zero))));
-        //onView(withText(R.string.elements_zero)).inRoot(withDecorView(not( is(mainActivityActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.et_operations), isDisplayed())).check(matches(hasErrorText(mainActivityActivityTestRule.getActivity().getBaseContext().getString(R.string.elements_zero))));
     }
+
     @Test
     public void testInputThreadsEmptyError() throws InterruptedException {
-        onView(withId(R.id.view_pager)).perform(ViewActions.swipeRight());
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft());
         Thread.sleep(500);
-        testInputOnTab("100000", " ");
+        testInputOnTab("6", " ");
         Thread.sleep(200);
-        onView(withId(R.id.et_threads)).check(matches(hasErrorText(String.valueOf(R.string.threads_empty))));
-        // onView(withText(R.string.threads_empty)).inRoot(withDecorView(not( is(mainActivityActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.et_operations), isDisplayed())).check(matches(hasErrorText(mainActivityActivityTestRule.getActivity().getBaseContext().getString(R.string.threads_empty))));
     }
+
     @Test
     public void testInputThreadsTooLittleError() throws InterruptedException {
-        onView(withId(R.id.view_pager)).perform(ViewActions.swipeRight());
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft());
         Thread.sleep(500);
-        testInputOnTab("100000", "0");
+        testInputOnTab("6", "0");
         Thread.sleep(200);
-        onView(withId(R.id.et_threads)).check(matches(hasErrorText(String.valueOf(R.string.threads_zero))));
-        //onView(withText(R.string.threads_zero)).inRoot(withDecorView(not( is(mainActivityActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.et_operations), isDisplayed())).check(matches(hasErrorText(mainActivityActivityTestRule.getActivity().getBaseContext().getString(R.string.elements_zero))));
     }
 }
 

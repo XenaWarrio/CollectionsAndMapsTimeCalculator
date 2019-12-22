649c58903abe1d59bc3fbfe03d489a53d26852a3
DanGdl <dan@superup.me>
2019-12-22 15:30:35 +0200

    # cleanup

--- app/src/androidTest/java/dx/queen/newcalculationandmaps/RecyclerViewMatcher.java
+++ app/src/androidTest/java/dx/queen/newcalculationandmaps/RecyclerViewMatcher.java
@@ -3,56 +3,47 @@ package dx.queen.newcalculationandmaps;
 import android.content.res.Resources;
 import android.view.View;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 public class RecyclerViewMatcher {
-    private final int recyclerViewId;
+    private final RecyclerView recyclerView;
 
-
-    public RecyclerViewMatcher(int recyclerViewId) {
-        this.recyclerViewId = recyclerViewId;
+    public RecyclerViewMatcher(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
     }
 
     public Matcher<View> atPosition(final int position) {
         return atPositionOnView(position, -1);
     }
 
-    public Matcher<View> atPositionOnView(final int position, final int targetViewId) {
-
+    private Matcher<View> atPositionOnView(final int position, final int targetViewId) {
         return new TypeSafeMatcher<View>() {
             Resources resources = null;
             View childView;
 
             public void describeTo(Description description) {
-                String idDescription = Integer.toString(recyclerViewId);
+                String idDescription = recyclerView.toString();
                 if (this.resources != null) {
                     try {
-                        idDescription = this.resources.getResourceName(recyclerViewId);
+                        idDescription = this.resources.getResourceName(recyclerView.getId());
                     } catch (Resources.NotFoundException var4) {
-                        idDescription = String.format("%s (resource name not found)",
-                                new Object[] { Integer.valueOf
-                                        (recyclerViewId) });
+                        idDescription = String.format("%s (resource name not found)", recyclerView);
                     }
                 }
 
-                description.appendText("with id: " + idDescription);
+                description.appendText("RecyclerView with id: " + idDescription + " at position: " + position);
             }
 
             public boolean matchesSafely(View view) {
-
                 this.resources = view.getResources();
-
                 if (childView == null) {
-                    RecyclerView recyclerView =
-                            (RecyclerView) view.getRootView().findViewById(recyclerViewId);
-                    if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
+                    if (recyclerView != null) {
                         childView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
-                    }
-                    else {
+                    } else {
                         return false;
                     }
                 }
@@ -60,10 +51,9 @@ public class RecyclerViewMatcher {
                 if (targetViewId == -1) {
                     return view == childView;
                 } else {
-                    View targetView = childView.findViewById(targetViewId);
+                    final View targetView = childView.findViewById(targetViewId);
                     return view == targetView;
                 }
-
             }
         };
     }
